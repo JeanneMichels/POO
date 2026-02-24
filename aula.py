5dfca80307f8f63266a9e2d12d930a1a0033883e
unknown <20241pvai@estudantes.ifpr.edu.br>
2026-02-24 10:01:02 -0300

    if else python

---
+++ aula.py
@@ -0,0 +1,20 @@
+#print('')
+#nome = input("nome: ")
+#idade = int(input("idade: "))
+#salario = float(input('salarioR$'))
+#print('Opa!', nome,'Ce ta bom?', 'oce tem', idade, 'anos, ta velho kk')
+#print('seu salario é: ', salario, 'ta pobre ksksk')
+
+#print(f"ola {nome} voce tem  {idade} anos")
+#if(salario == 10000 ):
+    #print(f"voce ganha muito mal, seu pobre. Seu salariop é R${salario: .2f} ")
+
+#elif(salario == 10000 or 20000):
+    ###print(f"voce ganha mal pra caralho kk R$: {salario: .2f}")
+
+print(f"digite um numero ")
+numero = int(input("numero:"))   
+if(numero > 100):
+    print(f"o numero foi dividido por dois:{numero // 2}")
+else:
+    print(f"o numero é: {numero}")
