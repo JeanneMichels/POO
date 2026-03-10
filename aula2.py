c062a0a640794825b4e67d34ce73cb7dae513e15
unknown <20241pvai0030002@estudantes.ifpr.edu.br>
2026-03-10 08:48:02 -0300

    exercicios dia 10/04/2026

---
+++ aula2.py
@@ -0,0 +1,29 @@
+#exibe os numeros 
+# numeros = [0] * 10
+# for cont in range(0, len(numeros)):
+#     numeros[cont] = int(input("informe um valor: "))
+# for cont in range(0,len(numeros)):
+#   print("valores: ", numeros[cont])
+
+#exibe nomes
+# alunos = [""] * 5
+# for cont in range(0,len(alunos)):
+#     alunos[cont] = input("informe um nome")
+# for cont in range(0, len(alunos)):
+#     print("nomes: ", alunos[cont])
+
+#conta de 1 a 20 
+# cont = 1
+# while(cont < 21):
+#     print("numeros: ", cont)
+#     cont = cont + 1
+
+# def somaValores (a, b):
+#     a = int(input("informe um numero: "))
+#     b = int(input("informe um numero: "))
+#     soma = a + b
+#     return soma
+
+
+
+
