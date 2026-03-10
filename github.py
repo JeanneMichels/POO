4a6eb587f367feefed9dc536365beaaf93f9d9c2
unknown <20241pvai0030002@estudantes.ifpr.edu.br>
2026-03-10 09:08:32 -0300

    exercicios dia 10/04/2026

--- github.py
+++ github.py
@@ -1,8 +1,14 @@
-coando_email = "git config user.email \"20241pvai0030002@estudantes.ifpr.edu.br\""
+import os
+comando_email = "git config user.email \"20241pvai0030002@estudantes.ifpr.edu.br\""
+os.system(comando_email)
 comando1 = "git add *"
+os.system(comando1)
 mensagem = input("mensagem do commit: ")
+
 while(len(mensagem) < 5):
     print("mensagem muito pequena, digite mais...😑")
     mensagem = input("mensagem do commit: ")
 comando2 = f"git commit -m {mensagem}"
+os.system(comando2)
 comando3 = "git push"
+os.system(comando3)
